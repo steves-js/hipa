a5c58e9c1e396838638b71b434e014d5c52a26bf
steves-js <n/a>
2018-12-17 21:57:11 -0500

    Initial commit with test push notification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hipa';
+
+  ngOnInit() {
+    Notification.requestPermission();
+  }
+
+  testNotification() {
+    if (Notification.permission === 'granted') {
+      const notification = new Notification('Hi there!');
+    } else if (Notification.permission !== 'denied') {
+      Notification.requestPermission().then((permission) => {
+        if (permission === 'granted') {
+          const notification = new Notification('Thank you for enabling me');
+        }
+      });
+    }
+   }
 }
